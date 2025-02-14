6630c97c752732b3a2e92f54eff7e9c82c7502e0
avargas <alevargasgarran@gmail.com>
2025-02-14 11:12:03 +0100

    Nuevo proyecto JPA

---
+++ Segundo trimestre/videoclub_jpa_rest-master/src/main/java/org/iesbelen/videoclub/domain/Idioma.java
@@ -0,0 +1,32 @@
+package org.iesbelen.videoclub.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name="idioma")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+//Si utilizo @OneToMany(FetchType.LAZY) además debo usar
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Idioma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_idioma")
+    private Long id;
+    private String nombre;
+
+    @OneToMany(mappedBy = "idioma")
+    @JsonIgnore
+    private List<Pelicula> peliculasIdioma;
+}
---
+++ Segundo trimestre/videoclub_jpa_rest-master/src/main/java/org/iesbelen/videoclub/domain/Pelicula.java
@@ -0,0 +1,48 @@
+package org.iesbelen.videoclub.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "pelicula")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pelicula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pelicula")
+    private long idPelicula;
+
+    private String titulo;
+
+    private String descripcion;
+
+    @Column(name = "anyo_lanzamiento")
+    @JsonFormat(pattern = "yyyy", shape = JsonFormat.Shape.STRING)
+    private Date anyoLanzamiento;
+
+    @ManyToOne()
+    @JoinColumn(name = "id_idioma", nullable = false)
+    private Idioma idioma;
+
+    private int duracion;
+
+    @ManyToMany
+    @JoinTable(
+            name = "pelicula_categoria",
+            joinColumns = @JoinColumn(name = "id_pelicula", referencedColumnName = "id_pelicula"),
+            inverseJoinColumns = @JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria"))
+    Set<Categoria> categorias = new HashSet<>();
+
+}
